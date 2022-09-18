17ac6888078b33c940a34e5b4f3d182d14c2c083
troubleNZ <54466974+troubleNZ@users.noreply.github.com>
2022-09-18 18:16:31 +1200

    version management
    no need to update for this

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,9 @@ fx_version 'bodacious'
 game 'gta5'
 
 description     'Easy Weapon License Approval'
-version         '1.0.0'
-
+version         '1.0.1'
+author          'TroubleshooterNZ'
+repository      'https://github.com/troubleNZ/trbl-LicensePed'
 
 client_script   'client.lua'
 
--- server.lua
+++ server.lua
@@ -19,3 +19,35 @@ AddEventHandler('pedlicense:server:GivePedWeaponLicense', function(selection)
     end 
 
 end)
+
+
+
+
+CreateThread(function(resourceName)
+    local resourceName <const> = GetCurrentResourceName()
+    PerformHttpRequest('https://raw.githubusercontent.com/troubleNZ/trbl-versions/main/trbl-LicensePed.json', function (errorCode, resultData, resultHeaders)
+      if not resultData then return end
+      local retData <const> = json.decode(resultData)
+      local version <const> = retData["version"]
+      local currentVersion <const> = GetResourceMetadata(resourceName, "version", 0)
+      local upToDateMsg <const> = retData["up-to-date"]["message"]
+      local updateMsg <const> = retData["requires-update"]["message"]
+      if version ~= currentVersion then
+        local updMessage <const> = "^3 - Update here: " .. GetResourceMetadata(resourceName, "repository", 0) .. " (current: v" .. currentVersion .. ", newest: v" .. version .. ")^0"
+        if retData["requires-update"]["important"] and updateMsg ~= nil then
+          print("")
+          print("  ^1Important Message:^0")
+          print("")
+          print((updateMsg):format(resourceName))
+          print(updMessage)
+          print("")
+          print("")
+        elseif updateMsg ~= nil then
+          print((updateMsg):format(resourceName) .. "^0")
+          print(updMessage)
+        end
+      elseif upToDateMsg ~= nil then
+        print((upToDateMsg):format(resourceName) .. "^0")
+      end
+    end, 'GET')
+  end)
